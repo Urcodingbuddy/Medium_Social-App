82fa790f6eb75de25facec88c3081791de6120e7
Urcodingbuddy <anshpethe110@gmail.com>
2024-09-19 12:07:55 +0530

    removed vite-copy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,6 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'dist/index.html',
-          dest: '', // This will copy `index.html` to the root of the output directory
-        },
-      ],
-    }),
-  ],
-  build: {
-    outDir: 'dist', // Ensure the output directory is correct
-  },
-})
+  plugins: [react()]
+});
